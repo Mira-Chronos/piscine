a96ded2863cf0eb29fee4ebf971949c23687fd72
William <stevens.wrc@gmail.com>
2025-12-03 15:59:19 +0100

    renaming list

--- piscine.py
+++ piscine.py
@@ -6,7 +6,11 @@
 
 print("--- Gestionnaire d'utilisateurs d'une piscine ---")
 
-liste = [("Léa","brasse",15,"25-11-24"),
+liste_nageurs = []
+
+liste_nages = []
+
+liste_bdd = [("Léa","brasse",15,"25-11-24"),
         ("Pierre","brasse",9,"25-11-24"),
         ("Michel","crawl",8,"25-11-26"),
         ("Léa","crawl",10,"25-11-25"),
@@ -24,40 +28,40 @@ while commande != 'exit':
         nage = input("quelle nage ? ")
         longueur = input("combien de longueur ? ")
         date = input("Quel jour ? YY_MM_DD")
-        liste.append((personne,nage,longueur,date))
+        liste_bdd.append((personne,nage,longueur,date))
    
     if commande == 'liste':
-        for personne, nage, longueur, date in liste:
+        for personne, nage, longueur, date in liste_bdd:
             print(f"Prénom {personne}, nage {nage}, longueur {longueur},Date {date}")
 
     if commande == 'nage':
         n=input("Quelle nage ?")
-        for personne, nage, longueur, date in liste:
+        for personne, nage, longueur, date in liste_bdd:
             if nage == n:
                 print(f"Prénom {personne},longueur {longueur},Date {date}")
 
     if commande == 'nageur':
         na = input("Qui?")
-        for personne, nage, longueur, date in liste:
+        for personne, nage, longueur, date in liste_bdd:
             if personne == na:
                 print(f"Nage {nage},longueur {longueur},Date {date}")
 
     if commande == 'date':
         da=input("Quelle date?")
-        for personne, nage, longueur, date in liste:
+        for personne, nage, longueur, date in liste_bdd:
             if date == da:
                 print(f"Prénom {personne},nage {nage},longueur {longueur}")
 
     if commande == 'save':
   
-        for elt in liste:
+        for elt in liste_bdd:
             with open(nom_fichier, 'w',  encoding='utf-8') as f:
-                    for personne, nage, longueur, date in liste:
+                    for personne, nage, longueur, date in liste_bdd:
                         f.write(f"{personne},{nage},{longueur},{date}\n")
         print(f"Liste sauvegardée ! dans '{nom_fichier}'.")
 
     if commande == 'load':
-        liste.clear()
+        liste_bdd.clear()
         with open(nom_fichier, 'r', encoding='utf-8') as fichier:
             for ligne in fichier:
                 ligne = ligne.strip()
@@ -69,4 +73,4 @@ while commande != 'exit':
                     longueur = int(longueur_str) 
                     
                     print(f"Prénom: {prenom}, Nom: {nom}, Longueur: {longueur}, Date: {date}")
-                    liste.append((prenom,nom,longueur,date))
+                    liste_bdd.append((prenom,nom,longueur,date))
